aaece27c5ee4913db87bc57283f8df4a482113ad
Ruslan Igorevich <r.borodavkin@eteron.ru>
2023-10-25 21:10:10 +0300

    added connect to DB

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -2,12 +2,14 @@
 
 import { useState, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
+import { usePathname, useRouter } from "next/navigation";
 import * as z from "zod";
 import Image from "next/image";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validations/user";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -35,6 +37,8 @@ function AccountProfile({ user, btnTitle }: Props) {
 
     const [files, setFiles] = useState<File[]>([]);
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -75,11 +79,25 @@ function AccountProfile({ user, btnTitle }: Props) {
         if (!hasImageCnanged) return;
 
         const imgRes = await startUpload(files);
-        if (imgRes && imgRes[0].fileUrl) {
-            values.profile_photo = imgRes[0].fileUrl;
+
+        if (imgRes && imgRes[0].url) {
+            values.profile_photo = imgRes[0].url;
         }
 
-        // TODO: Update user profile
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname
+        });
+
+        if (pathname === '/profile/edit') {
+            router.back();
+        } else {
+            router.push('/');
+        }
     };
 
     return (
---
+++ lib/mongoose.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+// Variable to check if mongoose is connected
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set("strictQuery", true);
+
+    if (!process.env.MONGODB_URL) {
+        return console.error("MONGODB_URL not found");
+    }
+
+    if (isConnected) {
+        return console.log("Already connected to MongoDB");
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        isConnected = true;
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error);
+    }
+}
